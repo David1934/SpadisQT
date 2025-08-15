7fe6cd989256727432e8019c7656d805cbceb17f
David.Chen <David.Chen@adaps-ph.com>
2025-08-15 16:12:34 +0800

    update algo lib

--- main.cpp
+++ main.cpp
@@ -62,24 +62,6 @@ static void handleSignal(int signal)
     }
 }
 
-#if !defined(CONSOLE_APP_WITHOUT_GUI)
-static void getScreenResolution()
-{
-    qreal screenWidth = QGuiApplication::primaryScreen()->geometry().width();
-    qreal screenHeight = QGuiApplication::primaryScreen()->geometry().height();
-
-    qCritical() << "QGuiApplication Screen Resolution:" << screenWidth << "x" << screenHeight;
-    DBG_INFO("QGuiApplication Screen  resolution: %f X %f...\n", screenWidth, screenHeight);
-
-    QRect screenGeometry = QApplication::desktop()->geometry();
-    int screenWidth2 = screenGeometry.width();
-    int screenHeight2 = screenGeometry.height();
-
-    //qCritical() << "QApplication Screen Resolution:" << screenWidth2 << "x" << screenHeight2;
-    DBG_INFO("QApplication Screen  resolution: %d X %d...\n", screenWidth2, screenHeight2);
-}
-#endif
-
 int main(int argc, char *argv[])
 {
     int result;
@@ -91,13 +73,6 @@ int main(int argc, char *argv[])
 
     if (!lockFile.tryLock(100))
     {
-#if 0 //!defined(CONSOLE_APP_WITHOUT_GUI)
-        QMessageBox msgBox;
-        msgBox.setIcon(QMessageBox::Warning);
-        msgBox.setText("The application is already running.\n"
-            "Allowed to run only one instance of the application.");
-        msgBox.exec();
-#endif
         DBG_NOTICE("An instance of the application is already running!");
 
         return 1;
@@ -115,17 +90,13 @@ int main(int argc, char *argv[])
     std::signal(SIGUSR1, handleSignal);
     std::signal(SIGUSR2, handleSignal);
     std::signal(SIGTSTP, handleSignal);
-    DBG_INFO("sizeof(BOOL): %ld, sizeof(float): %ld, sizeof(double): %ld, sizeof(long): %ld, sizeof(long long): %ld", sizeof(BOOL), sizeof(float), sizeof(double), sizeof(long), sizeof(long long));
 
 #if !defined(CONSOLE_APP_WITHOUT_GUI)
     w.setWindowFlags(w.windowFlags() & ~Qt::WindowMaximizeButtonHint & ~Qt::WindowMinimizeButtonHint);
-    //w.setStyleSheet("QMainWindow::title { font-family: Arial; font-size: 21pt; }");
     w.show();
-    getScreenResolution();
 #endif
 
     result = a.exec();
-    DBG_INFO("Application exited with code:: %d", result);
 
     return result;
 }
